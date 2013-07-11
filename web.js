59a96d951bb43064d4a2b0dc2de325e6b35b4e65
Ryan Simon <ryan@techacez.com>
2013-07-11 16:39:36 -0700

    changes to web.js

--- web.js
+++ web.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 var message = Buffer.toString(fs.readFileSync('index.html'));
 
 app.get('/', function(request, response) {
